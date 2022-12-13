3aaa288f99249cea48b80262a492e202852be4b2
Laetiwan <llimouzineau@gmail.com>
2022-12-13 11:02:29 +0100

    projet

--- controllers/sauces.js
+++ controllers/sauces.js
@@ -11,12 +11,13 @@ exports.createSauce = (req, res, next) => {
         imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
         likes: 0,
         dislikes: 0,
-        usersLiked: [' '],
-        usersDisliked: [' '],
+        usersLiked: [''],
+        usersDisliked: [''],
     });  
     sauce.save()
     .then(() => { 
-        res.status(201).json({message: 'Objet enregistré !'});      
+        res.status(201).json({message: 'Objet enregistré !'});  
+        console.log(sauceObject);    
     })
     .catch(error => { res.status(400).json( { error })})
 };
@@ -25,33 +26,65 @@ exports.likeSauce = (req, res, next) => {
     const userId = req.body.userId;
     console.log(userId);
     const like = req.body.like;
-    console.log(like);
-    
+    console.log(like);    
+        
     if(like === 1) {
-        Sauce.updateOne({ _id: req.params.id}, {$inc: {likes: + 1}, $push: {usersLiked: userId}})
-            .then(() => res.status(200).json({message : 'Objet modifié!'}))
-            .catch(error => res.status(401).json({ error })); 
-    }
+        Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            console.log(sauce.usersLiked);
+            if ((sauce.usersLiked.includes(userId))) {
+                res.status(401).json({ message : 'Not authorized'});
+            } 
+            else {
+                Sauce.updateOne({ _id: req.params.id}, {$inc: {likes: + 1}, $push: {usersLiked: userId}})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+                console.log(sauce.usersLiked);
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
+    }    
 
     if(like === -1) {
-        Sauce.updateOne({ _id: req.params.id}, {$inc: {dislikes: + 1}, $push: {usersDisliked: userId}})
-          .then(() => res.status(200).json({message : 'Objet modifié!'}))
-          .catch(error => res.status(401).json({ error }));
+        Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if ((sauce.usersDisliked.includes(userId))) {
+                console.log(sauce.usersDisliked);
+                res.status(401).json({ message : 'Not authorized'});
+            } 
+            else {
+                Sauce.updateOne({ _id: req.params.id}, {$inc: {dislikes: + 1}, $push: {usersDisliked: userId}})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });
     }
-
+    
     if (like === 0) {
-        Sauce.updateOne({ _id: req.params.id }, {$inc: {likes: -1}, $pull: {usersLiked: userId}})
-            .then(() => {
-                return Sauce.updateOne (
-                    { _id: req.params.id },
-                    { $inc: { dislikes: +1 }, $pull: { usersDisliked: userId } }
-                );
-            })
-            .then(() => {
-                res.status(201).json({ message: ['Like annulé', 'Dislike annulé'] });
-            })
-            .catch((error) => res.status(400).json(error));
-    }    
+        Sauce.findOne({_id: req.params.id})
+        .then((sauce) => {
+            if ((sauce.usersLiked.includes(userId))) {
+                console.log(sauce.usersLiked);
+                Sauce.updateOne({ _id: req.params.id}, {$inc: {likes: - 1}, $pull: {usersLiked: userId}})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            } 
+            if ((sauce.usersDisliked.includes(userId))) {
+                console.log(sauce.usersDisliked);
+                Sauce.updateOne({ _id: req.params.id}, {$inc: {dislikes: - 1}, $pull: {usersDisliked: userId}})
+                .then(() => res.status(200).json({message : 'Objet modifié!'}))
+                .catch(error => res.status(401).json({ error }));
+            }
+        })
+        .catch((error) => {
+            res.status(400).json({ error });
+        });                
+    }               
 };
 
 exports.modifySauce = (req, res, next) => {
